f1f9f1733a2b49adf834ea438f0b6ca696c7cfd1
Serik Dyssukov <lunabright.linaria@icloud.com>
2025-08-19 22:26:53 +0500

    update travel_recommendation_api.json path to relative instead of absolute

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ if (searchBtn !== null) {
 
         showLoading();
 
-        fetch('/data/travel_recommendation_api.json')
+        fetch('../data/travel_recommendation_api.json')
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok " + response.statusText);
